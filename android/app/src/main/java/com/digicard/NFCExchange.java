412db78dd00ef5a67d2423edca6d4edadb357c8c
Liu jinlong <q641480816@gmail.com>
2017-11-14 11:27:43 +0800

    add s nfc module

--- android/app/src/main/java/com/digicard/NFCExchange.java
+++ android/app/src/main/java/com/digicard/NFCExchange.java
@@ -1,13 +1,52 @@
 package com.digicard;
 
+import android.content.Context;
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class NFCExchange extends AppCompatActivity {
+import com.digicard.util.MNdefMessage;
+
+public class NFCExchange extends AppCompatActivity implements NfcAdapter.CreateNdefMessageCallback{
+
+    private final String url = "www.card-digi.com/#/download?id=";
+    private Context mContext;
+    private NfcAdapter mNfcAdapter;
+    private Intent intent;
+    private String id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfcexchange);
+
+        //init
+        mContext = this;
+        intent = getIntent();
+        id = intent.getStringExtra("id");
+        bindView();
+        setListeners();
+
+        //setNFC
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(mContext);
+        mNfcAdapter.setNdefPushMessageCallback(this, this);
+    }
+
+    private void bindView(){
+
+    }
+
+    private void setListeners(){
+
+    }
+
+    @Override
+    public NdefMessage createNdefMessage(NfcEvent nfcEvent) {
+        String url = this.url + this.id;
+        NdefMessage message = MNdefMessage.getNdefMsg_from_RTD_URI(url,(byte)0x03,false);
+        return message;
     }
 }
--- android/app/src/main/java/com/digicard/nativeModule/TryNfc.java
+++ android/app/src/main/java/com/digicard/nativeModule/TryNfc.java
@@ -1,15 +1,14 @@
 package com.digicard.nativeModule;
 
 import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.provider.Settings;
 
 import com.digicard.NFCExchange;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-
-/**
- * Created by Gods on 11/6/2017.
- */
 
 public class TryNfc extends ReactContextBaseJavaModule {
     public TryNfc(ReactApplicationContext reactContext) {
@@ -22,8 +21,35 @@ public class TryNfc extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void invoke(String id) {
-        Intent it = new Intent(getReactApplicationContext(), NFCExchange.class);
-        getCurrentActivity().startActivity(it);
+    public void invoke(String id, Promise promise) {
+        NfcAdapter mNfcAdapter;
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(getReactApplicationContext());
+        try {
+            if(mNfcAdapter == null){
+                promise.reject("0", "NFC Not Supported");
+            }else{
+                if(!mNfcAdapter.isEnabled()){
+                    promise.reject("1", "NFC Is off");
+                }else{
+                    if(!mNfcAdapter.isNdefPushEnabled()){
+                        promise.reject("2", "Android Beam Push Is Off");
+                    }else{
+                        promise.resolve("Pushing mother fucker!!!");
+                        Intent it = new Intent(getReactApplicationContext(), NFCExchange.class);
+                        it.putExtra("id",id);
+                        getReactApplicationContext().startActivity(it);
+                    }
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            promise.reject("3",e.getMessage());
+        }
+    }
+
+    @ReactMethod
+    public void goAndroidPushSetting() {
+        Intent setNfc = new Intent(Settings.ACTION_NFCSHARING_SETTINGS);
+        getCurrentActivity().startActivity(setNfc);
     }
 }
